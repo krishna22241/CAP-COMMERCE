c802338a38262c084d4c5e1cf8f8cc45ff58b35a
krishna22241 <krishnapath224@gmail.com>
2024-06-01 23:31:10 +0530

    added all files

---
+++ client/src/hooks/useCategory.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+const API_URL = process.env.REACT_APP_API || "";
+export default function useCategory() {
+  const [categories, setCategories] = useState([]);
+
+  //get cat
+  const getCategories = async () => {
+    try {
+      const { data } = await axios.get(`${API_URL}/api/v1/category/get-category`);
+      setCategories(data?.category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return categories;
+}
---
+++ client/src/pages/Contact.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
+const Contact = () => {
+  return (
+    <Layout title={"Contact us"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
+          <p className="text-justify mt-2">
+            buy anything at any time
+          </p>
+          <p className="mt-3">
+            <BiMailSend /> : www.group.project.com
+          </p>
+          <p className="mt-3">
+            <BiPhoneCall /> : 
+          </p>
+          <p className="mt-3">
+            <BiSupport /> : 0000000000
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
