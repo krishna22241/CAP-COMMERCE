56e1dc13b78de9a60d5cfd78ef10554e1b6d3212
krishna22241 <krishnapath224@gmail.com>
2024-06-01 23:54:03 +0530

    Build project for deployment

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -85,7 +85,7 @@ const ProductDetails = () => {
           {relatedProducts?.map((p) => (
             <div className="card m-2" key={p._id}>
               <img
-                src={`/api/v1/product/product-photo/${p._id}`}
+                src={`${API_URL}/api/v1/product/product-photo/${p._id}`}
                 className="card-img-top"
                 alt={p.name}
               />
